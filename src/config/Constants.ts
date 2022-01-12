eb61b92a60a4dc2fd025b0a06256436503228815
Heitor Valenca <heitorvalenca7@gmail.com>
2022-01-12 18:03:37 -0300

    created the app

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://heitorvalenca:<password>@cluster0.rells.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
